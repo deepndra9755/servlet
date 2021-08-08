325b933dc02267890aaa3828f23476082291b84b
cotlin <cotlin@DESKTOP-RNJ3PV0>
2021-08-07 19:25:40 -0700

    init parameter

---
+++ SettingINITParamUsingAnotation/src/com/servlet/prectice/AnotationINITDemo.java
@@ -0,0 +1,38 @@
+package com.servlet.prectice;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/up",initParams =@WebInitParam(name ="v2",value ="from init anotation param"))
+public class AnotationINITDemo extends HttpServlet {
+	
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter pw=response.getWriter();
+		response.setContentType("text/html");
+		String id=request.getParameter("id");
+		pw.println(id);
+		pw.println(getInitParameter("v2"));
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
