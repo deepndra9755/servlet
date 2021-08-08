e1aeddbd5d129b613f4e62024ff0ffcdc42f0ec2
cotlin <cotlin@DESKTOP-RNJ3PV0>
2021-08-07 18:59:48 -0700

    FAQ demo proj

---
+++ FAQDEmo/src/com/servlet/prectice/MultipleNameDEmo.java
@@ -0,0 +1,37 @@
+package com.servlet.prectice;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MultipleNameDEmo extends HttpServlet {
+Connection con1=null;
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter pw=response.getWriter();
+		response.setContentType("text/html");
+		String id=request.getParameter("id");
+		pw.println(id);
+		//pw.println(getInitParameter("first"));
+	
+		ServletContext k12=getServletContext();
+		
+		pw.println(k12.getInitParameter("first"));
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+
+}
